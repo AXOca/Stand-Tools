6f0059ca9e5b488d5ffd780e2a0b7645beafb166
Kodama <96012806+AXOca@users.noreply.github.com>
2024-06-20 21:57:46 +0200

    Edited gitignore

--- StandSupportTool-cs/src/StandSupportTool/obj/Debug/net8.0-windows/App.g.i.cs
+++ StandSupportTool-cs/src/StandSupportTool/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace StandSupportTool {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace StandSupportTool {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
         public static void Main() {
             StandSupportTool.App app = new StandSupportTool.App();
             app.InitializeComponent();
--- StandSupportTool-cs/src/StandSupportTool/obj/Debug/net8.0-windows/StandSupportTool.AssemblyInfo.cs
+++ StandSupportTool-cs/src/StandSupportTool/obj/Debug/net8.0-windows/StandSupportTool.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StandSupportTool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a42a7f58380153c7fd11f45b4bc96bcc456d188a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac0be7316037bca83beb2889c2c9aa151fc5c3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StandSupportTool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StandSupportTool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
